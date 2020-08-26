88e5997ca47da7d4fae25182596c502ee8ff44d8
Dan Schmitz <dan@televort.com>
2020-08-25 22:38:40 -0500

    Added Destination Bar and Slash Command
    Hitting the / key displays a destination bar: an input field with a button. Clicking the button or pressing the return key calls GoToNewDestination from the GUIController script.
    
    This will load take a user to a different location by loading a new scene or moving their position elsewhere.

---
+++ Assets/Scripts/GUIController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GUIController : MonoBehaviour
+{
+    public GameObject destinationInputFieldGameObject;
+    public InputField destinationInputField;
+    public string commandInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        destinationInputFieldGameObject.SetActive(false);
+        //commandInput = destinationInputField.text;
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp("/")) {
+            DisplayDestinationBar();
+        }
+    }
+
+    public void DisplayDestinationBar() {
+        Debug.Log("Destination bar displayed.");
+        destinationInputFieldGameObject.SetActive(true);
+        destinationInputField.Select();
+        Debug.Log("Input field selected.");
+    }
+
+    public void PrintCommandInput() {
+        Debug.Log("Sent message: " + commandInput);
+    }
+
+    public void GoToNewDestination() {
+        commandInput = destinationInputField.text;
+        Debug.Log("Televorting to " + commandInput);                  // Find a scene by name or tag that the user inputs. Load that scene asynchronously.
+    }
+
+
+
+}
