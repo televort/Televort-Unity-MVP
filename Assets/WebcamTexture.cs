ea46cbcae742c73e94073799f55f23006a544e98
Dan Schmitz <dan@televort.com>
2020-08-27 14:17:59 -0500

    Added webcam texture to Sandbox scene

---
+++ Assets/WebcamTexture.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class WebcamTexture : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        WebCamTexture webcamTexture = new WebCamTexture();
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = webcamTexture;
+        webcamTexture.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
